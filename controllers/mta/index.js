fd0d0379211273d001136f375a96d07743b094b1
Joel Rosario <YOUR EMAIL ADDRESS>
2017-04-06 14:09:39 -0400

    pushing data

--- controllers/mta/index.js
+++ controllers/mta/index.js
@@ -2,7 +2,8 @@ const router = require('express').Router();
 const controller = require('./controller');
 
 router.get('/', controller.index);
-// router.get('/:id', controller.show);
-// router.get('/edit/:id', controller.edit);
+router.get('/:id', controller.show);
+router.get('/edit/:id', controller.edit);
+
 
 module.exports = router;
--- models/mta.js
+++ models/mta.js
@@ -6,18 +6,19 @@ mtas.findAll = () => {
   return db.any('SELECT * FROM trains');
 };
 
-// movies.showOne = (id) => {
-//   return db.one('SELECT * FROM movie WHERE id =$1', [id])
-// }
+mtas.showOne = (id) => {
+  return db.one('SELECT * FROM trains WHERE id =$1', [id])
+}
 
-// movies.create = (title, poster, rating, runtime) => {
-//     return db.one('INSERT INTO movie(title, poster, rating, runtime) VALUES ($1, $2, $3, $4) returning id', [title, poster, rating, runtime]);
-// };
-// movies.update = (id, title, poster, runtime, rated) => {
-//     return db.one('UPDATE movie SET title = $1, poster =$2, runtime = $3, rating = $4 WHERE id = $5 returning id', [title, poster, runtime, rated, id]);
-// };
-// movies.destroy = (id) => {
-//     return db.none('DELETE FROM movie WHERE id = $1', [id])
-// }
+mtas.create = (ca, unit, scp, station, linename, division, dates, event_time,description,entries,exits) => {
+    return db.one('INSERT INTO trains(ca, unit, scp, station, linename, division, dates, event_time,description,entries,exits) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) returning id', [ca, unit, scp, station, linename, division, dates, event_time,description,entries,exits]);
+};
+mtas.update = (id, ca, unit, scp, station, linename, division, dates, event_time,description,entries,exits) => {
+    return db.one('UPDATE trains SET ca = $1, unit =$2, scp = $3, station = $4, linename = $5, division = $6, dates = $7, event_time = $8, description = $9, entries = $10, exits = $11,  WHERE id = $12 returning id', [ca, unit, scp, station, linename, division, dates, event_time,description,entries,exits, id]);
+};
+
+mtas.destroy = (id) => {
+    return db.none('DELETE FROM trains WHERE id = $1', [id])
+}
 
 module.exports = mtas;
