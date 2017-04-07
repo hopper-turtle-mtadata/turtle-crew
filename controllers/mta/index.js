e443741211391180fffd5c2a8648cff00e6f02de
Joel Rosario <YOUR EMAIL ADDRESS>
2017-04-07 13:30:46 -0400

    joel

--- controllers/mta/index.js
+++ controllers/mta/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const controller = require('./controller');
 
 router.get('/', controller.index);
-router.get('/:id', controller.show);
+// router.get('/:id', controller.show);
 router.post('/station', controller.dropdown);
 
 module.exports = router;
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,103 +1,68 @@
 $(function () {
 
-  $('#stations').keyup(()=>{
-    $.ajax({
-      url: 'station',
-      method: 'POST',
-      data: {station: $("#stations").val()},
-      success: data => {
-        $(".drop").text(data.station);
-      }, error: err => {
-        console.log("err", err);
-      }
-    })
+  const list = $('.list');
+
+  list.click(function(){
+     $('#UserInput').empty();
+     $('#UserInput').val(this.innerText);
+     $('.list').hide();
   });
-// Dropdown ends
-
-
-  
 
 
 
+  // const $clickApendr = $('.list').on('click', function(e) {
+  //   console.log(e);
+  //   let m = $(e.target).parent();
+  //   console.log(m);
+  //   let children = m.children();
+  //   let name = $(children).text();
+  // })
 
+// var list = $('.list');
+//
 
+//   list.click(function(){
+//    $('#UserInput').empty();
+//    $('#UserInput').val(list.text);
+//    list.empty();
+//
+// });
 
+//set up good just get the json values
+const Calling = (album) => {
 
-     // Step 3. Create a data object
-     var data = {
-         labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
-         datasets: [
-             {
-                 label: "The First Dataset",
-                 fillColor: "rgba(153,0,76,0.2)", // magenta
-                 strokeColor: "rgba(153,0,76,1)", // magenta
-                 pointColor: "rgba(153,0,76,1)", // magenta
-                 pointStrokeColor: "#fff", // white
-                 pointHighlightFill: "#fff", // white
-                 pointHighlightStroke: "rgba(153,0,76,1)", // magenta
-                 data: [100, 34, 21, 56, 23, 90, 40]
-             },
-             {
-                 label: "The Second dataset",
-                 fillColor: "rgba(76,0,153,0.2)",
-                 strokeColor: "rgba(76,0,153,1)",
-                 pointColor: "rgba(76,0,153,1)",
-                 pointStrokeColor: "#fff", // white
-                 pointHighlightFill: "#fff", // white
-                 pointHighlightStroke: "rgba(76,0,153,1)",
-                 data: [28, 48, 40, 19, 86, 27, 90]
-             }
-         ]
-     };
-   var option = {
-     ///Boolean - Whether grid lines are shown across the chart
-     scaleShowGridLines : true,
+    const albumContainer = $('<div></div>').addClass('albumContainer');
+    result = $('<div class="result">').text(album.Result);
+    lat = $('<div class="lat">').text(album.latitude);
+    long = $('<div class="long">').text(album.longitude);
+    $('.albumContainer').remove();
+     result.appendTo(albumContainer);
+     lat.appendTo(albumContainer);
+     long.appendTo(albumContainer);
+    $('#resultsContainer').append(albumContainer);
+  }
 
-     //String - Colour of the grid lines
-     scaleGridLineColor : "rgba(0,0,0,.05)",
+  $('#searchSubmit').on('click', event => {
+      const UserInputVal = $('#UserInput').val();
+      const Alldays = $('#Days').val();
+      const hoursVal = $('#hours').val();
+        console.log(UserInputVal);
+        console.log(Alldays);
+        console.log(hoursVal);
 
-     //Number - Width of the grid lines
-     scaleGridLineWidth : 1,
+    // http://localhost:5000/predict?STATION=%221%20AV%22&DAY=%22Monday%22&TIME=5
+    console.log('http://127.0.0.1:5000/predict?STATION='+ UserInputVal + '&DAY='+ Alldays + '&TIME='+ hoursVal);
 
-     //Boolean - Whether to show horizontal lines (except X axis)
-     scaleShowHorizontalLines: true,
-
-     //Boolean - Whether to show vertical lines (except Y axis)
-     scaleShowVerticalLines: true,
-
-     //Boolean - Whether the line is curved between points
-     bezierCurve : true,
-
-     //Number - Tension of the bezier curve between points
-     bezierCurveTension : 0.4,
-
-     //Boolean - Whether to show a dot for each point
-     pointDot : true,
-
-     //Number - Radius of each point dot in pixels
-     pointDotRadius : 4,
-
-     //Number - Pixel width of point dot stroke
-     pointDotStrokeWidth : 1,
-
-     //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
-     pointHitDetectionRadius : 20,
-
-     //Boolean - Whether to show a stroke for datasets
-     datasetStroke : true,
-
-     //Number - Pixel width of dataset stroke
-     datasetStrokeWidth : 2,
-
-     //Boolean - Whether to fill the dataset with a colour
-     datasetFill : true,
- };
-
-     // Step 2. Get the context of the canvas element we want to select
-     var ctx = document.getElementById("myChart").getContext("2d");
-     var myNewChart = new Chart(ctx).Line(data, option);
-
-
+    $.ajax({
+      method: 'GET',
+      url: 'http://127.0.0.1:5000/predict?STATION='+ UserInputVal + '&DAY='+ Alldays + '&TIME='+ hoursVal,
+      dataType: 'JSON',
+      success: function(data) {
+        Calling(data);
+      }
+    }); //end of AJAX call
+//#searchSubmit end of click
+});
 
 
 
