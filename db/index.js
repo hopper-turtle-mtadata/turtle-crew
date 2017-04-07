2be143c8210b480a204cdc571081c158bd86921a
Paul Sanabria <pandres.sanabria@gmail.com>
2017-04-07 10:29:47 -0400

    clean branch

--- db/index.js
+++ db/index.js
@@ -3,6 +3,6 @@ const pgp = require('pg-promise')();
 const db = pgp(
   process.env.DATABASE_URL
   ||
-  'postgres://samanthagaraicoa@localhost:5432/mtadata');
+  'postgres://paulsanabria@localhost:5432/mtadata');
 
   module.exports = db;
